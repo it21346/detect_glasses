10ea213dcadc29d1961c67258d75908248929817
it21346 <steliosbombai@gmail.com>
2023-11-07 11:22:14 +0100

    Loading the ActiveLoop dataset. In addition, I resize the dataset to a specific size and create a new branch on Activeloop.

---
+++ load_dataset_activeloop.py
@@ -0,0 +1,58 @@
+import deeplake 
+from deeplake.util.exceptions import TransformError, CheckoutError
+import tensorflow as tf
+import numpy as np
+import albumentations as A
+import cv2
+from PIL import Image
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+
+# My ActiveLoop token
+api_token = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTY5NzU0ODU3MSwiZXhwIjoxNzI5MTcwOTUwfQ.eyJpZCI6Iml0MjEzNDYifQ.nAQNToa3w0eFPOWBxtAUzreKdZipPuLZ6Oxrw5ynbGPrrtti-8PayEzazX2U6dp48cT5zjffsTK-nMr9Z2XfrA"
+resize_size = (224,224)
+
+def load_dataset():
+    """
+    Load the dataset from ActiveLoop
+    """
+    ds = deeplake.load('hub://it21346/glasses_or_not_dataset', token = api_token)
+    return ds
+
+
+def resize_dataset_activeloop(ds, resize_size):
+    """
+    There is a need to resize the original images of the dataset, which are (1024, 1024, 3) size, to an appropriate size for feeding our later on NN model.
+    """
+    try: #if the resized dataset branch already exists, do nothing and move on. Otherwise, create a resized dataset branch.
+        ds = deeplake.load('hub://it21346/glasses_or_not_dataset@resized/dataset', token = api_token)
+    except CheckoutError as e:
+
+        
+
+        @deeplake.compute
+        def resize(sample_in, sample_out, new_size):
+            
+            sample_out.labels.append(sample_in.labels.numpy())
+            sample_out.images.append(np.array(Image.fromarray(sample_in.images.numpy()).resize(new_size)))
+            
+            return sample_out
+
+        try:
+            resize(new_size = resize_size).eval(ds, num_workers = 4)
+        except TransformError as e:
+            failed_idx = e.index
+            failed_sample = e.sample
+            print(failed_idx)
+
+        
+        ds.commit(f"Resize Dataset to {resize_size}")
+
+    return ds
+
+if __name__ == "__main__":
+    ds = load_dataset()
+    ds = resize_dataset_activeloop(ds)
+    if ds.branch != 'main': #it 
+        #load a specific branch, like the resized one
+        ds = deeplake.load(f'hub://it21346/glasses_or_not_dataset@{ds.branch}', token = api_token)
