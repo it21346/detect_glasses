c6a534cb3124b527a0cc8981187068aa26b7f275
it21346 <steliosbombai@gmail.com>
2023-11-07 12:53:49 +0100

    Added argparse in order to be called from command line with specific arguments (Token, Data Dir, ActiveLoop repo)

--- create_dataset_activeloop.py
+++ create_dataset_activeloop.py
@@ -1,15 +1,30 @@
 import deeplake 
 import os 
-
+import argparse
 
 def create_dataset(token, dataset_dir, Activeloop_repo):
     # Activeloop token
     os.environ['ACTIVELOOP_TOKEN'] = token
 
 
-    # Directory of the dataset
+    # Directory of the dataset (./Images/Images)
     data_directory = dataset_dir
     # Activeloop repo URL
     deeplake_path = Activeloop_repo
 
     deeplake.ingest_classification(data_directory, deeplake_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create the dataset into Activeloop")
+    #Arguments
+
+    parser.add_argument('--token', help="ActiveLoop API Token")
+    parser.add_argument('--dir', help= "Dataset directory. (./Images/Images)")
+    parser.add_argument('--repo', help= "ActiveLoop repository URL")
+
+    args = parser.parse_args()
+
+    token = args.token
+    dir = args.dir
+    repo = args.repo
+    create_dataset(token, dir, repo)
